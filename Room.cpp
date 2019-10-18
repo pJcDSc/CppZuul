b89871c5731a96b7bd123ad23da5f6b5346f5a37
U-BSD\411036 <411036@SHS-10DEC4F83.beaverton.k12.or.us>
2019-10-18 09:10:32 -0700

    Finishing Room.Cpp

--- Room.cpp
+++ Room.cpp
@@ -6,6 +6,29 @@ Room Class
 
 #include <iostream>
 #include <cstring>
+#include <vector>
 #include "Room.h"
 #include "Item.h"
 
+using namespace std;
+
+//Constructor
+Room::Room(char* n, char* d) {
+  this -> name = n;
+  this -> desc = d;
+}
+
+//Get Items
+vector<Item*>* Room::getItems() {
+  return itemList;
+}
+
+//Get Exits
+map<char*, Room*>* Room::getExits() {
+  return exits;
+}
+
+//Get Exit locked
+bool Room::getLocked(char* c) {
+  return exitLocked[c];
+}
--- Room.h
+++ Room.h
@@ -6,6 +6,7 @@ Room Class Header
 
 #include <iostream>
 #include <cstring>
+#include <vector>
 #include "Item.h"
 
 using namespace std;
@@ -20,30 +21,41 @@ class Room {
   //Get Items
   vector<Item*>* getItems();
 
-  //Get Exit
-  Room getExit(char*);
-
   //Remove Items
-  Item removeItem(Item);
+  Item* removeItem(Item*);
 
   //Add Item
-  void addItem(Item);
+  void addItem(Item*);
 
   //Set Exit
-  Room setExit(char*, Room);
+  Room* setExit(char*, Room*);
 
   //Get Name
   char* getName();
 
   //Get Description
   char* getDescription();
+
+  //Toggle locked exit
+  bool toggleLock(char*);
+
+  //Get Exits
+  map<char*, Room*>* getExits();
+
+  //Get Exit locked
+  bool getLocked(char*);
+
+  //Get Exit Description
+  char* getExitDesc(char*);
   
  private:
 
-  vector<Item*> v;
+  vector<Item*> v; //Vector for itemList
   vector<Item*>* itemList;
-  map<char*, Room> exits;
-  char* description;
+  map<char*, Room*>* exits;
+  map<char*, char*>* exitDescs;
+  map<char*, bool>* exitLocked;
+  char* desc;
   char* name;
 
 };
